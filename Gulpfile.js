0455415de4c59d5b269a0029936e772665332f69
CMilne <cmilne@ideo.com>
2016-02-21 18:51:30 -0500

    Finished Tutorial

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,29 @@ autoprefixer = require('gulp-autoprefixer'),
 minifycss = require('gulp-minify-css'),
 rename = require('gulp-rename');
 
+gulp.task('express', function() {
+	var express = require('express');
+	var app = express();
+	app.use(require('connect-livereload')({port: 35729}));
+	app.use(express.static(__dirname));
+	app.listen(4000, '0.0.0.0');
+});
+
+var tinylr;
+gulp.task('livereload', function() {
+	tinylr = require('tiny-lr')();
+	tinylr.listen(35729);
+});
+
+function notifyLiveReload(event) {
+	var fileName = require('path').relative(__dirname, event.path);
+	tinylr.changed({
+		body: {
+			files: [fileName]
+		}
+	});
+}
+
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
 	.pipe(sass().on('error', sass.logError))
@@ -15,17 +38,12 @@ gulp.task('styles', function() {
 	.pipe(gulp.dest('./css/'));
 });
 
-gulp.task('express', function() {
-	var express = require('express');
-	var app = express();
-	app.use(express.static(__dirname));
-	app.listen(4000, '0.0.0.0');
-});
-
 gulp.task('watch', function() {
-  gulp.watch('sass/*.scss', ['styles']);
+	gulp.watch('sass/*.scss', ['styles']);
+	gulp.watch('*.html', notifyLiveReload);
+	gulp.watch('css/*.css', notifyLiveReload);
 });
 
-gulp.task('default', ['express', 'watch'], function() {
+gulp.task('default', ['styles', 'express', 'livereload', 'watch'], function() {
 
 });
